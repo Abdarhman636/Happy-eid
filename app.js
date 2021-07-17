6db4a2b64e30261fbc63741cbca59956cf0dafc0
Abdarhman <abdarhmanalsyaed@yahoo.com>
2021-07-17 18:06:24 +0300

    canvase

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 var namee = document.getElementById('name')
-var printname = document.getElementById('printname')
+const downloadBtn = document.getElementById('download-btn')
+// var printname = document.getElementById('printname')
 
-namee.onkeyup = function () {
-     document.getElementById('printname').innerHTML = namee.value;
+// namee.onkeyup = function () {
+//      document.getElementById('printname').innerHTML = namee.value;
+// }
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+// var nameInput = getElementById('nameput')
+
+const img = new Image()
+img.src = 'main-img.png'
+img.onload = function () {
+     drawImage()
 }
+
+function drawImage() {
+     ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+     ctx.font = '1.8rem ammar'
+     ctx.fillStyle = '#0b2a4a'
+     ctx.fillText(namee.value, 250, 500)
+}
+
+namee.addEventListener('input', function () {
+     const name = namee.value
+     drawImage()
+})
+
+
+downloadBtn.addEventListener('click', function () {
+     downloadBtn.href = canvas.toDataURL()
+     downloadBtn.download = "تهنئة عيد" + namee.value
+})
